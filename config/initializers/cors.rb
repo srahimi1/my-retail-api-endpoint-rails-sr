eaf80b1adc60a12ab1fd0823208a5dfb16b7b29f
unknown <watid2005@aim.com>
2021-07-27 16:02:54 -0400

    updated allowed origin url in cors.rb file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://my-retail-api-consumer-site.herokuapp.com'
+    origins 'https://my-retail-api-consumer-site-sr.herokuapp.com'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
